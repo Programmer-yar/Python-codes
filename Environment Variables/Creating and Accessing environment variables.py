2a0ff72053a5d1fa9be07dfe6ee98d8cbb2978a7
yar <ahmadyar228@gmail.com>
2021-08-16 16:55:17 +0500

    environment variables on ubuntu and windows

--- Environment Variables/Creating and Accessing environment variables.py
+++ Environment Variables/Creating and Accessing environment variables.py
@@ -1,8 +1,16 @@
 """
+Windows:
 First we need to create environment variables in windows
 1- Go to My computer icon Properties by right clicking
 2- In the top left side click on "advanced system settings"
-3- click on "environment variables" and create "User variables"
+3- click on "environment variables" and create "User variables".
+"""
+"""
+Ubuntu/linux:
+Create environment variables in "/etc/environment" file.
+1- Open "/etc/environment" using "vim" or "nano" editor as you like
+2- store variable: <variable_name>="<value>"
+3- Restart ubuntu or logout and login back
 """
 """
 To access the environment variables follow the following method
@@ -10,10 +18,12 @@ To access the environment variables follow the following method
 
 import os
 
-user_id = os.environ.get('USER_ID')
+test_id = os.environ.get('test_id')
 #USER_ID was already defined as environment variable
 #get method used in "dictionaries" to get 'value' of a 'key'
-user_password = os.environ.get('user_password')
+user_password = os.environ.get('test_pass')
+test_var = os.environ.get('test_var')
 
-print(user_id)
-print(user_password)
+print("test_id", test_id)
+print("test_pass" ,user_password)
+print("test variable: ", test_var)
--- listop.py
+++
@@ -1,8 +0,0 @@
-nums=[10,9,8,7,6,5]
-nums[0]=nums[1]-5
-if 4 in nums:
-    print(nums[3])
-else:
-    print(nums[4])
-print(not 4 in nums)
-print(4 not in nums)
