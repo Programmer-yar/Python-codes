4479d90a97115aa19ab684d4a0372a3347eec4bd
yar <ahmadyar228@gmail.com>
2021-12-10 15:30:59 +0500

    seperated advanced python topics like asyncio

--- Advanced Python/(__name__ == __main__)/second_module.py
+++
@@ -1,3 +0,0 @@
-import first_module
-
-print(f'Second Module name: {__name__}')
--- Advanced Python/subprocess_learning.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-output = subprocess.run("dir", capture_output=True, shell=True, text=True)
-isinstance(output, str)
-
-#print(output)
-#subprocess.run(['cmd', '/c', "dir"])
